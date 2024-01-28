30493188004c0ce81a8396dae619abd7dee63206
MaryRdriz <157423930+MaryRdriz@users.noreply.github.com>
2024-01-28 14:55:43 -0600

    Tarea evaluativa Sesión 2 Mary Rodriguez López

---
+++ Dias de semana.py
@@ -0,0 +1,5 @@
+Dias = ["Lunes","martes","Miercoles","Jueves","Viernes","Sabado","domingo"]
+
+numeros = int(input("ingrese un numero del 1 al 7 para mostrar el dia correspondiente de la semana:  "))
+
+print(Dias[numeros-1])
---
+++ Numeros del 1 al 100.py
@@ -0,0 +1,10 @@
+print("ingrese un valor para la realización de la suma:")
+n = int(input())
+s = 0
+
+for i in range(1, n+1):
+    print(i)
+    s = s+i
+
+
+print("la suma es:", s)
